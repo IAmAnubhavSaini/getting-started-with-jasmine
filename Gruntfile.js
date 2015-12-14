902516733b069c8d1da98b1a7eb07f526fc64fd6
Anubhav Saini <IAmAnubhavSaini@gmail.com>
2015-12-14 10:05:53 +0530

    setting up gruntfile | SASS | step 3.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,14 @@ module.exports = function(grunt) {
     grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     sass: {
-
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'src/app.css': 'src/app.scss'
+        }
+      }
     },
     jshint: {
 
@@ -16,7 +23,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-  
-  grunt.registerTask('default', [""]);
+
+  grunt.registerTask('default', ["sass"]);
 
 };
